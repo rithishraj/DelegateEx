24545ac43de72b91096f19da93ac8da5eb38b944
ELCOT <ELCOT@LENOVO>
2022-12-05 17:44:36 +0530

    Add project files.

---
+++ DelegateEx/ConcatEx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateEx
+{
+    public static class ConcatEx
+    {
+        public static string concatstr(this string fn)
+        {
+         string re = fn.ToUpper();
+            return re ;
+        }
+    }
+}
---
+++ DelegateEx/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//example of extension method and delegation
+namespace DelegateEx
+{
+    //delegate int MyDel(int x, int y);
+
+    internal class Program
+    {
+        //static int Add (int x,int y)
+        //{
+        //    return x + y;
+        //}
+        //static int Mul(int x,int y)
+        //{
+        //    return x * y;
+        //}
+        static void Main()
+        {
+            string fin = "rithish";
+            string Res = fin.concatstr();
+            
+            Console.WriteLine(Res); 
+            //MyDel del = new MyDel(Add);
+            
+            //int res = del(1, 2);
+            //int res2= del.Invoke(12, 12);
+
+            //Console.WriteLine(res);
+           
+
+            //Console.WriteLine(res2);
+            Console.ReadKey();
+
+
+        }
+    }
+}
